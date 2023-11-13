96bc629ea25c33b0e114506f815ea69155b5c391
Mario Guilherme Wilhelms <marioguilhermew@gmail.com>
2023-11-13 09:30:27 -0300

    Correcao camelCase

--- schemas/index.js
+++ schemas/index.js
@@ -3,11 +3,23 @@ import horarios from "./horarios";
 import noticias from "./noticias";
 import duvidas from "./duvidas";
 import laboratorios from "./laboratorios";
-import descricao_metodos from "./metodo";
+import descricaoMetodos from "./metodo";
 import clube from "./clube";
 import equipe from "./equipe";
 import premios from "./premios";
 import preclube from "./preClube";
-import diasdasemana from "./horariosDiasDaSemana"
+import diasDaSemana from "./horariosDiasDaSemana"
 
-export const schemaTypes = [horarios,certificados,noticias,duvidas,equipe, premios, descricao_metodos, laboratorios, clube, preclube,diasdasemana]
+export const schemaTypes = [
+    horarios,
+    certificados,
+    noticias,
+    duvidas,
+    equipe,
+    premios,
+    descricaoMetodos,
+    laboratorios,
+    clube,
+    preclube,
+    diasDaSemana
+]
