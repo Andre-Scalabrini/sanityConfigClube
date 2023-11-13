8de2b8b634fe14cacfad4805e559606390d19ed6
MarioGuiW <marioguilhermew@gmail.com>
2023-11-13 13:49:49 -0300

    teste

---
+++ schemas/premios copy.js
@@ -0,0 +1,25 @@
+// schemas/pet.js
+export default {
+    name: 'dsadsadas',
+    type: 'document',
+    title: 'Prêmios',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Título'
+      },
+      {
+        name: 'description',
+        type: 'string',
+        title: 'Descrição'
+      },
+      {
+        name: 'imagem',
+        type: 'image',
+        title: 'Imagem'
+      }
+    ]
+  }
+  
+  
